3e0abd008a35cf2e21ee79c128f51e7c877de932
Zenith_Avro <abuhasan323382@gmail.com>
2025-08-03 21:16:41 +0600

    more dicssuion an Asynce

--- 04-Advanced/Fetch.js
+++ 04-Advanced/Fetch.js
@@ -1,3 +1,42 @@
+// Async/await is a powerful feature in JavaScript that provides a cleaner,more readable way to work with asynchrounous operation.
+
+// It's build on top of Promises and makes asynchronous code look and behave more like synchronous code.
+
+
+//when you declare a function as async, it automatically returns a Promises 
+
+async function example(){
+  return 'Hello World';
+}
+
+function example2(){
+  return Promise.resolve("Hello World")
+}
+
+example().then(result => console.log(result))
+
+
+
+
+// Example 
+
+async function fetchUserData() {
+    try {
+        const userResponse = await fetch('/api/user');
+        const user = await userResponse.json();
+        
+        const postsResponse = await fetch(`/api/posts/${user.id}`);
+        const posts = await postsResponse.json();
+        
+        return { user, posts };
+    } catch (error) {
+        console.error('Error:', error);
+    }
+}
+
+
+
+
 fetch("https://randomuser.me/api/")
   .then((rawdata) => {
     return rawdata.json();
---
+++ 05-DOM/Basic.js
@@ -0,0 +1,3 @@
+let  h3 = document.querySelector('h3')
+
+h3.textContent = "Hello Youtuber"
